e57c5ee00dcf76abd6d4815236b63f7683f69ade
hafizjavaid <hafizjaidi786@gmail.com>
2024-07-17 17:03:34 +0500

    First: Basic Setup

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <q-btn color="primary" label="Primary" />
+  <QBtn color="secondary" label="Secondary" />
+  <LazyQBtn color="amber" glossy label="Amber" />
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,62 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import materialIcons from 'quasar/icon-set/svg-material-icons'
+import materialIconsRound from 'quasar/icon-set/svg-material-icons-round'
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  modules: ["nuxt-quasar-ui", "@nuxt/content"],
+  quasar: {
+
+    plugins: [
+      'AppFullscreen',
+      'BottomSheet',
+      'Dialog',
+      'Loading',
+      'LoadingBar',
+      'Notify',
+    ],
+    sassVariables: true,
+    iconSet: {
+      ...materialIcons,
+      colorPicker: materialIconsRound.colorPicker,
+    },
+    extras: {
+      font: 'roboto-font',
+      fontIcons: ['material-icons'],
+      animations: 'all',
+    },
+    appConfigKey: 'nuxtQuasarCustom',
+    config: {
+      dark: false,
+      brand: {
+        // primary: '#ff0000',
+      },
+    },
+    components: {
+      defaults: {
+        QBtn: {
+          color: 'primary',
+        },
+        QLinearProgress: {
+          color: 'green',
+          size: '15px',
+          stripe: true,
+        },
+        QCircularProgress: {
+          color: 'blue',
+          indeterminate: true,
+        },
+        QSelect: {
+          outlined: true,
+          dense: true,
+        },
+        QInput: {
+          outlined: true,
+        },
+        QToggle: {
+          color: 'red',
+        },
+      },
+    },
+  }
+})
