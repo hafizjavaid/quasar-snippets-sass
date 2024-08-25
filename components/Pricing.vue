e0bf483a05d303ac26f4de985081fa10db99908e
hafizjavaid <hafizjaidi786@gmail.com>
2024-08-25 14:11:22 +0500

    Finalizing Pricing

--- components/Pricing.vue
+++ components/Pricing.vue
@@ -7,17 +7,18 @@
                 personal or commercial projects without any additional fees. </div>
             <div class="q-py-xl">
 
-                <div class="row">
-                    <div class="col-12 col-sm-6">
-                        <q-card color="grey-2"  bordered flat style="border-radius: 20px;" class="q-pa-lg bg-grey-2">
+                <div class="row items-center ">
+                    <div class="col-12 col-sm-6" style="z-index: 2;" >
+                        <q-card color="grey-2" bordered  style="border-radius: 20px;" class="q-pa-lg bg-grey-2">
                             <div class="flex justify-between q-mb-sm">
                                 <div class="text-primary text-weight-medium text-body1">Get with all-access</div>
                                 <q-btn padding="4px 10px" unelevated no-caps label="Get all-access" dense color="dark"
                                     style="border-radius: 8px;"></q-btn>
                             </div>
                             <div class="flex items-center q-gutter-x-sm">
-                                <div class="text-h4">
-                                    $<span class="text-weight-bold">299</span>
+                                <div class="text-h3">
+                                    <span class="text-strike ">$299</span>
+                                    <span class="text-weight-bold q-ml-sm">$199</span>
                                 </div>
                                 <div>
                                     <div class="text-weight-bold">one-time payment</div>
@@ -25,44 +26,131 @@
                                 </div>
                             </div>
                             <q-card-section class="q-px-none">
-                                <div class="text-body1" >
+                                <div class="text-body1">
 
-                                    Get lifetime access to all of the application UI, marketing, and ecommerce components,
+                                    Get lifetime access to all of the application UI, marketing, and ecommerce
+                                    components,
                                     as well as all of our site templates for a single one-time purchase.
                                 </div>
                             </q-card-section>
 
-                            <q-separator class="q-my-lg"></q-separator>
+                            <q-separator class="q-my-xl"></q-separator>
 
                             <div class="flex items-center  no-wrap q-gutter-x-md  q-mb-xl">
                                 <q-icon size="40px" name="img:/icons/box.svg" color="primary">
                                 </q-icon>
-                                <div class="text-body1" >
-                                    <span class="text-weight-bold" >Over 500+ components</span>
-                                    — everything you need to build beautiful application UIs, marketing sites, ecommerce stores, and more.
+                                <div class="text-body1">
+                                    <span class="text-weight-bold">Over 500+ components</span>
+                                    <br /> everything you need to build beautiful application UIs, marketing sites, ecommerce
+                                    stores, and more.
                                 </div>
                             </div>
                             <div class="flex items-center  no-wrap q-gutter-x-md  q-mb-xl">
                                 <q-icon size="40px" name="img:/icons/template.svg" color="primary">
                                 </q-icon>
-                                <div class="text-body1" >
-                                    <span class="text-weight-bold" >Every site template</span>
-                                    — beautifully designed, expertly crafted website templates built with modern technologies like Quasar and Nuxt.
+                                <div class="text-body1">
+                                    <span class="text-weight-bold">Every site template</span>
+                                    <br /> beautifully designed, expertly crafted website templates built with modern
+                                    technologies like Quasar and Nuxt.
                                 </div>
                             </div>
                             <div class="flex items-center  no-wrap q-gutter-x-md">
                                 <q-icon size="40px" name="img:/icons/inifinity.svg" color="primary">
                                 </q-icon>
-                                <div class="text-body1" >
-                                    <span class="text-weight-bold" >Lifetime access</span>
-                                    — Your purchase gives you lifetime access to all current and future Quasar UI components. Use them in personal or commercial projects without any additional fees.
+                                <div class="text-body1">
+                                    <span class="text-weight-bold">Lifetime access</span>
+                                    <br /> Your purchase gives you lifetime access to all current and future Quasar UI
+                                    components. Use them in personal or commercial projects without any additional fees.
                                 </div>
                             </div>
 
                         </q-card>
                     </div>
                     <div class="col-12 col-sm-6">
+                        <q-card bordered flat style="border-radius: 20px;" class="q-pa-lg">
+                            <div>
+                                <div class="flex justify-between items-center">
+                                    <div class="text-weight-medium text-body1 text-primary ">Application UI</div>
+                                    <q-btn padding="4px 10px" unelevated no-caps label="" dense color="grey-4" round
+                                        outline style="border-radius: 8px;">
+                                        <template #default>
+                                            <div class="text-dark">Unlock Package</div>
+                                        </template>
+                                    </q-btn>
+                                </div>
+                                <div class="flex items-center q-gutter-x-sm">
+                                    <div class="text-h4">
+                                        <span class="text-strike ">$149</span>
+                                        <span class="text-weight-bold q-ml-sm">$99</span>
+                                    </div>
+                                    <div>
+                                        <div>plus local taxes</div>
+                                    </div>
+                                </div>
+                                <q-card-section class="q-px-none">
+                                    <div class="text-body1">
+                                        Gain access to all of the QuasarUI components in the Application UI category — all
+                                        of
+                                        the components you need to
+                                        build out your application's UI.
+                                    </div>
+                                </q-card-section>
+                            </div>
+                            <q-separator class="q-my-lg"></q-separator>
+                            <div>
+                                <div class="flex justify-between items-center">
+                                    <div class="text-weight-medium text-body1 text-primary ">Marketing</div>
+                                    <q-btn padding="4px 10px" unelevated no-caps label="" dense color="grey-4" round
+                                        outline style="border-radius: 8px;">
+                                        <template #default>
+                                            <div class="text-dark">Unlock Package</div>
+                                        </template>
+                                    </q-btn>
+                                </div>
+                                <div class="flex items-center q-gutter-x-sm">
+                                    <div class="text-h4">
+                                        <span class="text-strike ">$149</span>
+                                        <span class="text-weight-bold q-ml-sm">$99</span>
+                                    </div>
+                                    <div>
+                                        <div>plus local taxes</div>
+                                    </div>
+                                </div>
+                                <q-card-section class="q-px-none">
+                                    <div class="text-body1">
+                                        Gain access to all of the QuasarUI components in the Marketing category — all of the components you need to build out your marketing pages.
+                                    </div>
+                                </q-card-section>
+                            </div>
+                            <q-separator class="q-my-lg"></q-separator>
 
+                            <div>
+                                <div class="flex justify-between items-center">
+                                    <div class="text-weight-medium text-body1 text-primary ">Ecommerce</div>
+                                    <q-btn padding="4px 10px" unelevated no-caps label="" dense color="grey-4" round
+                                        outline style="border-radius: 8px;">
+                                        <template #default>
+                                            <div class="text-dark">Unlock Package</div>
+                                        </template>
+                                    </q-btn>
+                                </div>
+                                <div class="flex items-center q-gutter-x-sm">
+                                    <div class="text-h4">
+                                        <span class="text-strike ">$149</span>
+                                        <span class="text-weight-bold q-ml-sm">$99</span>
+                                    </div>
+                                    <div>
+                                        <div>plus local taxes</div>
+                                    </div>
+                                </div>
+                                <q-card-section class="q-px-none">
+                                    <div class="text-body1">
+                                        Gain access to all of the QuasarUI components in the E-commerce category — all of the components you need to build out your e-commerce pages.
+                                    </div>
+                                </q-card-section>
+                            </div>
+
+                        </q-card>
                     </div>
 
                 </div>
