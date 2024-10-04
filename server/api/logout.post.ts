894a16f3e76d198aa42ac2a0f2b967196f2f9b14
hafizjavaid <hafizjaidi786@gmail.com>
2024-10-04 11:56:40 +0500

    Removing Extra code from server side

--- server/api/logout.post.ts
+++ server/api/logout.post.ts
@@ -1,9 +1,3 @@
 export default defineEventHandler(async (event) => {
-	if (!event.context.session) {
-		throw createError({
-			statusCode: 403
-		});
-	}
-	await lucia.invalidateSession(event.context.session.id);
-	appendHeader(event, "Set-Cookie", lucia.createBlankSessionCookie().serialize());
+	
 });
